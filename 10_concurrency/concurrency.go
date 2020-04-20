e695d56ac530ae7ff6aca061c1b21af6c34ea486
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-04-21 00:03:24 +0200

    Solve performance issue with channels.
    -- Benchmark results:
    ```
    goos: linux
    goarch: amd64
    pkg: github.com/eduardostalinho/go_with_tests/10_concurrency
    BenchmarkCheckWebsites-4              37          38551297 ns/op
    ```

--- 10_concurrency/concurrency.go
+++ 10_concurrency/concurrency.go
@@ -1,13 +1,27 @@
 package concurrency
+type result struct {
+	string
+	bool
+}
 
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChan := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChan <- result{u, wc(u)}
+		}(url)
 	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChan
+		results[result.string] = result.bool
+	}
+
+	return results
 
 	return results
 }
