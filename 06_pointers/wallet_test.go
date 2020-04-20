4430f77ff7327296a88c6a680242b26bcc175f2c
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-04-20 14:40:16 +0200

    Implement withdraw.

--- 06_pointers/wallet_test.go
+++ 06_pointers/wallet_test.go
@@ -3,15 +3,29 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(20))
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(10)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(0)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
 
 func TestBitcoinStringer(t *testing.T) {
