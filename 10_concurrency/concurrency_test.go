fb407198eeb7047ec78abb20cd7ade2d1fd77c20
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-04-21 00:01:03 +0200

    Add benchmark test.
    -- Benchmark Results
    ```
    goos: linux
    goarch: amd64
    pkg: github.com/eduardostalinho/go_with_tests/10_concurrency
    BenchmarkCheckWebsites-4               1        2022042981 ns/op
    ```

--- 10_concurrency/concurrency_test.go
+++ 10_concurrency/concurrency_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 const errorWebsiteURL = "waat://invalidurl"
@@ -28,5 +29,22 @@ func TestCheckWebsites(t *testing.T) {
 
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
+
+
+func slowWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowWebsiteChecker, urls)
 	}
 }
