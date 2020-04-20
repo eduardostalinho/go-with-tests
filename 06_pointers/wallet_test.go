15ab0cbcba904c023d9370bfed1d552aa0e32558
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-04-20 14:32:25 +0200

    Implements Stringer interface on Bitcoin type.

--- 06_pointers/wallet_test.go
+++ 06_pointers/wallet_test.go
@@ -13,3 +13,16 @@ func TestWallet(t *testing.T) {
 		t.Errorf("got %d want %d", got, want)
 	}
 }
+
+func TestBitcoinStringer(t *testing.T) {
+	btc := Bitcoin(10)
+
+	want := "10 BTC"
+
+	got := btc.String()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+
+}
