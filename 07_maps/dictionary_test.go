4bb396410a90fcc6ae0f36faa9540f584cac4370
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-04-20 15:46:29 +0200

    Isolate assert function.

--- 07_maps/dictionary_test.go
+++ 07_maps/dictionary_test.go
@@ -8,8 +8,13 @@ func TestSearch(t *testing.T) {
 	got := Search(dictionary, "test")
 	want := "this is a test"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got string, want string) {
+	t.Helper()
+
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
-
 }
