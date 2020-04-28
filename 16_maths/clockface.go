7878d77cd7e4adcd3fd3d1d255449239b8020710
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-04-28 20:00:05 +0200

    Add logic able to rotate the second pointer.

--- 16_maths/clockface.go
+++ 16_maths/clockface.go
@@ -1,14 +1,34 @@
 package clockface
 
 import (
+	"math"
 	"time"
 )
 
 type Point struct {
-	X int
-	Y int
+	X float64
+	Y float64
 }
 
+const (
+	secondHandLength = 90
+	clockCentreX     = 150
+	clockCentreY     = 150
+)
+
 func SecondHand(tm time.Time) Point {
-	return Point{150, 60}
+	p := secondHandPoint(tm)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}
+	return p
+}
+
+func secondHandPoint(tm time.Time) Point {
+	angle := secondInRadians(tm)
+	return Point{math.Sin(angle), math.Cos(angle)}
+}
+
+func secondInRadians(tm time.Time) float64 {
+	return math.Pi / (30 / float64(tm.Second()))
 }
--- 16_maths/clockface_test.go
+++ 16_maths/clockface_test.go
@@ -1,18 +1,92 @@
 package clockface
 
 import (
+	"fmt"
+	"math"
 	"testing"
 	"time"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-	want := Point{X: 150, Y: 150 - 90}
+func TestSecondHand(t *testing.T) {
+	t.Run("at midnight", func(t *testing.T) {
+		tm := simpleTime(0, 0, 0)
+		want := Point{X: 150, Y: 150 - 90}
 
-	got := SecondHand(tm)
+		got := SecondHand(tm)
 
-	if got != want {
-		t.Errorf("got %v, wanted %v", got, want)
+		if got != want {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+	})
+
+	t.Run("at 30 seconds", func(t *testing.T) {
+		tm := simpleTime(0, 0, 30)
+		want := Point{X: 150, Y: 150 + 90}
+
+		got := SecondHand(tm)
+
+		if got != want {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+	})
+
+}
+func TestSecondHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 0, 30), Point{0, -1}},
+		{simpleTime(0, 0, 45), Point{-1, 0}},
+	}
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("at %d seconds", test.time.Second()), func(t *testing.T) {
+			got := secondHandPoint(test.time)
+			if !roughlyEqualPoint(got, test.point) {
+				t.Errorf("got %v, want %v", got, test.point)
+			}
+		})
+	}
+}
+
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) &&
+		roughlyEqualFloat64(a.Y, b.Y)
+}
+
+func TestSecondInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), math.Pi / 2 * 3},
+		{simpleTime(0, 0, 7), math.Pi / 30 * 7},
 	}
 
+	for _, test := range cases {
+		t.Run(testName(test.time), func(t *testing.T) {
+			want := test.angle
+
+			got := secondInRadians(test.time)
+			if got != want {
+				t.Errorf("got %v, wanted %v", got, want)
+			}
+		})
+	}
+}
+
+func simpleTime(h, m, s int) time.Time {
+	return time.Date(312, time.February, 5, h, m, s, 0, time.UTC)
+
+}
+
+func testName(t time.Time) string {
+	return fmt.Sprintf("%d:%d:%d", t.Hour(), t.Minute(), t.Second())
 }
