24e2c1b0dcba00b2e7c32be265181a93166a0c07
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-04-28 10:54:57 +0200

    Convert arabic numbers (type int) to roman.

---
+++ 15_property_based_tests/numerals_test.go
@@ -0,0 +1,35 @@
+package numerals
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestConverToRomans(t *testing.T) {
+	cases := []struct {
+		Arabic int
+		Roman  string
+	}{
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{7, "VII"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{47, "XLVII"},
+		{2020, "MMXX"},
+	}
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("convert %d to %s", test.Arabic, test.Roman), func(t *testing.T) {
+			got := ConvertToRomans(test.Arabic)
+
+			if got != test.Roman {
+				t.Errorf("got %s, want %s", got, test.Roman)
+			}
+		})
+	}
+}
