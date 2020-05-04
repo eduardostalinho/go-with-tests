1f6fd7ea1223cf0456c897d7df5caa029f8397ea
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-05-04 14:52:18 +0200

    Run formatter on old exercises.

--- 04_arrays/sum_test.go
+++ 04_arrays/sum_test.go
@@ -4,8 +4,9 @@ import (
 	"reflect"
 	"testing"
 )
+
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func (t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 		got := Sum(numbers)
 		want := 15
@@ -15,7 +16,7 @@ func TestSum(t *testing.T) {
 		}
 	})
 
-	t.Run("collection of any size", func (t *testing.T) {
+	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5, 6, 7}
 		got := Sum(numbers)
 		want := 28
