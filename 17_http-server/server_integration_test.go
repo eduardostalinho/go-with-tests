b3377a0798e1b44b5c33dd699f22eb017eac973f
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-05-06 11:30:43 +0200

    Use t.Run for integration test.

--- 17_http-server/server_integration_test.go
+++ 17_http-server/server_integration_test.go
@@ -9,20 +9,21 @@ import (
 func TestRecordWinsAndRetrieveScore(t *testing.T) {
 	dbPath := "testdb.bolt"
 	bucket := "scoresTest"
-	db := setupBolt(dbPath, bucket)
-	defer tearDownBolt(bucket, db)
 
 	store := NewBoltPlayerStore(dbPath, bucket)
-	server := &PlayerServer{store}
-	player := "Agnawd"
-	wins := 3
+	defer store.db.Close()
+	t.Run("record 3 wins and get score", func(t *testing.T) {
+		server := &PlayerServer{store}
+		player := "Agnawd"
+		wins := 3
 
-	for i := 0; i < wins; i++ {
-		server.ServeHTTP(httptest.NewRecorder(), newPostScoreRequest(player))
-	}
+		for i := 0; i < wins; i++ {
+			server.ServeHTTP(httptest.NewRecorder(), newPostScoreRequest(player))
+		}
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
 
-	assertResponseBody(t, response.Body.String(), strconv.Itoa(wins))
+		assertResponseBody(t, response.Body.String(), strconv.Itoa(wins))
+	})
 }
