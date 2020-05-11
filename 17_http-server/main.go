d5b4f67ceeb84ce26010c3382314a2b29ee3779f
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-05-11 11:45:41 +0200

    Close bolt db on end of main.

--- 17_http-server/main.go
+++ 17_http-server/main.go
@@ -8,7 +8,9 @@ import (
 func main() {
 	boltPath := "scores.bolt"
 	bucketName := "scores"
+	store := NewBoltPlayerStore(boltPath, bucketName)
 	server := NewPlayerServer(NewBoltPlayerStore(boltPath, bucketName))
+	defer store.db.Close()
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not start server on port 5000. Error: %v", err)
 	}
