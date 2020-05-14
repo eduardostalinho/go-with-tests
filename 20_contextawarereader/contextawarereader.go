39453d2f37783edbaae739fe1224787446d8bd11
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-05-14 16:57:47 +0200

    Create cancellable reader using context.

--- 20_contextawarereader/contextawarereader.go
+++ 20_contextawarereader/contextawarereader.go
@@ -1,1 +1,22 @@
 package contextawarereader
+
+import (
+	"context"
+	"io"
+)
+
+type CancellableReader struct {
+	delegate io.Reader
+	ctx      context.Context
+}
+
+func (r *CancellableReader) Read(p []byte) (n int, err error) {
+	if err := r.ctx.Err(); err != nil {
+		return 0, err
+	}
+	return r.delegate.Read(p)
+}
+
+func NewCancellableReader(ctx context.Context, r io.Reader) io.Reader {
+	return &CancellableReader{delegate: r, ctx: ctx}
+}
--- 20_contextawarereader/contextawarereader_test.go
+++ 20_contextawarereader/contextawarereader_test.go
@@ -1,6 +1,7 @@
 package contextawarereader
 
 import (
+	"context"
 	"strings"
 	"testing"
 )
@@ -21,8 +22,53 @@ func TestContextAwareReader(t *testing.T) {
 
 		assertBufferHas(t, got, "DEF")
 	})
+	t.Run("context-aware reader behaves like normal reader", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		r := NewCancellableReader(ctx, strings.NewReader("ABCDEF"))
+		got := make([]byte, 3)
+
+		_, err := r.Read(got)
+
+		abortOnError(t, err)
+
+		assertBufferHas(t, got, "ABC")
+
+		_, err = r.Read(got)
+		abortOnError(t, err)
+
+		assertBufferHas(t, got, "DEF")
+		cancel()
+	})
+	t.Run("context-aware reader stops reading on cancel", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		r := NewCancellableReader(ctx, strings.NewReader("ABCDEF"))
+		got := make([]byte, 3)
+
+		_, err := r.Read(got)
+
+		abortOnError(t, err)
+
+		assertBufferHas(t, got, "ABC")
+
+		cancel()
+
+		n, err := r.Read(got)
+
+		expectError(t, err)
+
+		if n > 0 {
+			t.Errorf("expected nothing read, but read %d bytes", n)
+		}
+	})
 }
 
+func expectError(t *testing.T, err error) {
+	t.Helper()
+	if err == nil {
+		t.Error("expected an error")
+	}
+
+}
 func abortOnError(t *testing.T, err error) {
 	t.Helper()
 	if err != nil {
